00125701c18cfc5965c8347f0678403b2edda33c
Kevin <kevinvalmarquez@gmail.com>
2023-09-18 19:10:21 -0300

    dark mode

--- app.js
+++ app.js
@@ -55,3 +55,31 @@ document.querySelector("#enviar").addEventListener("click", (e) => {
     }
 
 })
+
+
+
+// DARK MODE
+
+const toggleSwitch = document.getElementById('theme-switch');
+const currentTheme = localStorage.getItem('theme');
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+    toggleSwitch.checked = true;
+  }
+}
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute('data-theme', 'dark');
+    localStorage.setItem('theme', 'dark');
+  }
+  else {
+    document.documentElement.setAttribute('data-theme', 'light');
+    localStorage.setItem('theme', 'light');
+  }
+}
+
+toggleSwitch.addEventListener('change', switchTheme, false);
